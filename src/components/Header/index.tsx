465f98f7c67d6e06d913d4989a39e56754b46274
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2023-07-03 13:37:58 -0300

    update Header (index.tsx)
    create Dialog with RadixUi

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo-ignite.svg';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
     return (
@@ -7,7 +8,21 @@ export function Header() {
         <HeaderContent>
           <img src={logoImg} alt="LogoIgnite" />
 
-          <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay />
+
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     )
